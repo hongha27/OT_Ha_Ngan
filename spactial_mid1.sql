c5da653f9fa6c62f1e744af8775f43a541a0b499
hà <tranthihongha321@gmail.com>
2019-11-13 23:08:28 +0700

    đã sửa

--- spactial_mid1.sql
+++ spactial_mid1.sql
@@ -17,7 +17,7 @@ INSERT INTO land VALUES(N'M7',N'Trồng lúa',75000,250.6,GEOMETRY::STPolyFromTe
 go
 INSERT INTO land VALUES(N'M8',N'Trồng cây ăn quả',45000,350.4,GEOMETRY::STPolyFromText('POLYGON((1 -3, 3 -3,3 -5, -2 -5,1 -4,1 -3))',4326))
 go
-INSERT INTO land VALUES(N'M9',N'Trồng rau',35000,450.5,GEOMETRY::STPolyFromText('POLYGON((1 0, 2 1,5 0, 5 -2,3 -3,1 -3, 1 0))',4326))
+INSERT INTO land VALUES(N'M9',N'Trồng rau',35000,450.5,GEOMETRY::STPolyFromText('POLYGON((1 0, 3 1,5 0, 5 -2,3 -3,1 -3, 1 0))',4326))
 go
 select * from land
 --Câu 2a: Cho biết lượng nước tưới cần thiết của mỗi loại đất.
